5579441c58e5cb2fc09a2c87f1c724b9e87bbc34
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-06-12 18:41:57 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,13 @@
+import turtle
+
+for i in range(2):
+    turtle.shape("square")
+    turtle.pencolor("red")
+    turtle.forward(200)
+    turtle.left(90)
+    
+for i in range(2):
+    turtle.shape("circle")
+    turtle.pencolor("green")
+    turtle.forward(200)
+    turtle.left(90)
